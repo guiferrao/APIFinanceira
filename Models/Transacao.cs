7cf303da08d1bf933303931f2e30da1ac41af47a
guiferrao <ferraogui@hotmail.com>
2026-01-20 18:40:10 -0300

    feat: Mapa relacional e conexao com o banco

---
+++ Models/Transacao.cs
@@ -0,0 +1,17 @@
+﻿namespace APIFinanceira.Models
+{
+    public class Transacao
+    {
+        public Guid Id { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime Data { get; set; }
+        public enum TipoTransacao
+        {
+            Entrada = 1,
+            Saida = 2
+        }
+        public TipoTransacao Tipo { get; set; }
+        public int UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+    }
+}
